dbaa5528b03748509e9775e52fc7e5d0f273368c
Igor Amantino <65914646+IgorGamantino@users.noreply.github.com>
2020-09-10 14:45:52 -0300

    DataBase Upload
    Desafio do GoStack Database NodeJS

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,64 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getRepository(Transaction);
+    const categoriesRepository = getRepository(Category);
+
+    const { total } = await getCustomRepository(
+      TransactionRepository,
+    ).getBalance();
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('Type is invalid!');
+    }
+
+    if (type === 'outcome' && value > total) {
+      throw new AppError('Can not outcome transaction without a valid balance');
+    }
+
+    let transactionCategory = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      value,
+      category_id: transactionCategory.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return {
+      ...transaction,
+      category: transactionCategory,
+    };
   }
 }
 
